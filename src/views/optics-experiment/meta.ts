65c541a2f73d1bf58e13eccd361b6d3fbd62e41a
Tạ Hữu Cường <dragonsvip090@gmail.com>
2026-03-14 15:38:23 +0700

    feat: add author facebook link to optics experiment meta

--- src/views/optics-experiment/meta.ts
+++ src/views/optics-experiment/meta.ts
@@ -4,6 +4,7 @@ const meta: PageMeta = {
   name: 'Thí nghiệm quang học',
   description: 'Mô phỏng các thí nghiệm với ánh sáng',
   author: 'iTCuong',
+  facebook: 'https://www.facebook.com/tahuucuong090/',
   category: 'learn',
 }
 
